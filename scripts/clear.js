a157f44592d18d6c9285e859a8475cc75f23e905
IvanDimovSIT <ivan390@abv.bg>
2022-10-11 14:09:59 +0300

    разширяване на нивото

--- scripts/clear.js
+++ scripts/clear.js
@@ -2,12 +2,17 @@ function clear(ctx, canvas, location){
 
     let gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
     
+    
+
     if(location.y > -1900){
         gradient.addColorStop(0, "rgb(200, 170, 100)");
         gradient.addColorStop(1, "rgb(180,50,10)");
-    }else{
+    }else if(location.y > -4800){
         gradient.addColorStop(0, "rgb(100, 100, 110)");
         gradient.addColorStop(1, "rgb(50,50,55)");
+    }else{
+        gradient.addColorStop(0, "rgb(10, 120, 50)");
+        gradient.addColorStop(1, "rgb(0,60,20)");
     }
 
     ctx.fillStyle = gradient;
--- scripts/game_objects.js
+++ scripts/game_objects.js
@@ -11,6 +11,10 @@ const wallImage2 = new Image();
 wallImage2.src = "assets/wall2.png";
 const wallPattern2 = ctx.createPattern(wallImage2, "repeat");
 
+const wallImage3 = new Image();
+wallImage3.src = "assets/wall3.png";
+const wallPattern3 = ctx.createPattern(wallImage3, "repeat");
+
 
 const walls = [
     new Wall( -600, 250, 1200, 200, wallPattern),
@@ -19,6 +23,7 @@ const walls = [
     new Wall(-350, -220, 650, 80, wallPattern),//middle
     new Wall( -1300, -2000, 100, 2100, wallPattern),//side walls
     new Wall( -1300, -4100, 100, 2100, wallPattern2),//side walls
+    new Wall( -1300, -6200, 100, 2100, wallPattern2),
     new Wall( 1200, -2000, 100, 2100, wallPattern),//side walls
     new Wall( -1000, -500, 600, 80, wallPattern),
     new Wall( -100, -700, 600, 80, wallPattern),
@@ -36,7 +41,33 @@ const walls = [
     new Wall( 1300, -2600, 600, 200, wallPattern2),
     new Wall( 600, -2900, 600, 400, wallPattern2),//savepoint2
     new Wall(-400, -3200, 500, 800, wallPattern2),
-    new Wall(300, -3500, 800, 100, wallPattern2)
+    new Wall(300, -3500, 800, 100, wallPattern2),
+    new Wall( 1400, -3700, 400, 100, wallPattern2),
+    new Wall( 1600, -3900, 200, 200, wallPattern2),
+    new Wall( 1100, -4100, 200, 100, wallPattern2),
+    new Wall( 800, -4200, 200, 100, wallPattern2),
+    new Wall( 500, -4300, 200, 100, wallPattern2),
+    new Wall( 650, -4600, 1200, 100, wallPattern2),
+    new Wall(-500, -6000, 100, 1900, wallPattern2),
+    new Wall( 1700, -4800, 1600, 200, wallPattern3),//start area3
+    new Wall( 3200, -7500, 100, 2700, wallPattern3),//right wall
+    new Wall( 1700, -7000, 100, 1800, wallPattern3),//left wall
+    new Wall( 1700, -7700, 1600, 200, wallPattern3),//roof
+    new Wall( 3100, -5150, 100, 100, wallPattern3),
+    new Wall( 1800, -5300, 300, 100, wallPattern3),
+    new Wall(1800, -5500, 100, 200, wallPattern3),
+    new Wall( 2100, -5800, 200, 50, wallPattern3),
+    new Wall( 2400, -5900, 80, 50, wallPattern3),
+    new Wall( 2600, -6000, 80, 50, wallPattern3),
+    new Wall( 2700, -6200, 100, 100, wallPattern3),
+    new Wall( 3100, -6400, 100, 300, wallPattern3),
+    new Wall( 1800, -6700, 100, 400, wallPattern3),
+    new Wall( 2200, -6600, 400, 100, wallPattern3),
+    new Wall( 1300, -7000, 400, 100, wallPattern3),
+    new Wall( 1200, -7800, 100, 900, wallPattern3),
+    new Wall( 1300, -7500, 50, 100, wallPattern3),
+    new Wall( 1300, -7200, 100, 100, wallPattern3),
+    new Wall( 1500, -7350, 300, 80, wallPattern3)
 ];
 
 const decorations = [
@@ -61,8 +92,14 @@ let coins = [
     new Coin(2200, -2800),
     new Coin(1300, -2900),
     new Coin(400, -3200),
-    new Coin(400, -3700)
+    new Coin(400, -3700),
+    new Coin(1600, -4100)
 ];
+for(let i = 0; i<3; i++){
+    for(let j=0; j<4; j++){
+        coins.push(new Coin(1200 + j*100, -4800-i*100))
+    }
+}
 
 const saws = [
     new Saw(2100, -1900),
@@ -81,12 +118,18 @@ const saws = [
     new Saw(-500, -3700),
     new Saw(-500, -3800),
     new Saw(-500, -3900),
-    new Saw(-500, -4000)
+    new Saw(-500, -4000),
+    new Saw(1200, -3600),
+    new Saw(1800, -4000),
+    new Saw(1800, -4100),
+    new Saw(1800, -4200),
+    new Saw(1600, -7700)
 ];
 
 const savePoints = [
     new SavePoint(1200, -2400),
-    new SavePoint(600, -3300)
+    new SavePoint(600, -3300),
+    new SavePoint(800, -5000)
 ];
 
 const screenBlur = new ScreenBlur();
